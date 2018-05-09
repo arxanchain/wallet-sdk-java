db347681979628db2b8df2df806b099c841eb54e
dongyan <dongyan@arxanfintech.com>
2018-05-09 12:48:54 +0800

    [T3670] move tomago.java to tomago-sdk-java project

--- src/test/java/com/arxanfintech/sdk/WalletTest.java
+++ src/test/java/com/arxanfintech/sdk/WalletTest.java
@@ -57,56 +57,52 @@ public class WalletTest extends TestCase {
      * Register Test
      */
     public void testRegister() {
-        //FIX ME
-        assertTrue(true);
-        
-//        String strdata = "{\"access\": \"92c62e1c-43ac-11e8-b377-186590cc5d35\", \"secret\": \"Integrate1230\", \"type\": \"Organization\", \"id\": \"\"}";
-//        JSONObject jsondata = JSON.parseObject(strdata);
-//
-//        String strheader = "{\"Callback-Url\":\"http://something.com\",\"Bc-Invoke-Mode\":\"sync\"}";
-//        JSONObject jsonheader = JSON.parseObject(strheader);
-//
-//        Client client = new Client();
-//        client.Address = "IP:PORT";
-//        client.ApiKey = "5zt592jTM1524126512";
-//        client.CertPath = "/Users/yan/tmp/cert136";
-//
-//        Wallet wallet = new Wallet(client);
-//
-//        try {
-//            wallet.Register(jsonheader, jsondata);
-//            assertTrue(true);
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//            assertTrue(false);
-//        }
+
+        String strdata = "{\"access\": \"92c62e1c-43ac-11e8-b377-186590cc5d35\", \"secret\": \"Integrate1230\", \"type\": \"Organization\", \"id\": \"\"}";
+        JSONObject jsondata = JSON.parseObject(strdata);
+
+        String strheader = "{\"Callback-Url\":\"http://something.com\",\"Bc-Invoke-Mode\":\"sync\"}";
+        JSONObject jsonheader = JSON.parseObject(strheader);
+
+        Client client = new Client();
+        client.Address = "IP:PORT";
+        client.ApiKey = "5zt592jTM1524126512";
+        client.CertPath = "/Users/yan/tmp/cert136";
+
+        Wallet wallet = new Wallet(client);
+
+        try {
+            wallet.Register(jsonheader, jsondata);
+            assertTrue(true);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            assertTrue(false);
+        }
     }
 
     /**
      * Balance Test
      */
     public void testBalance() {
-        //FIX ME
-        assertTrue(true);
-        
-//        String id = "did:axn:8d5c38a9-8ead-468d-b6d3-bec7d62341b0";
-//
-//        String strheader = "{\"Callback-Url\":\"http://something.com\"}";
-//        JSONObject jsonheader = JSON.parseObject(strheader);
-//
-//        Client client = new Client();
-//        client.Address = "IP:PORT";
-//        client.ApiKey = "5zt592jTM1524126512";
-//        client.CertPath = "/Users/yan/tmp/cert136";
-//
-//        Wallet wallet = new Wallet(client);
-//
-//        try {
-//            wallet.Balance(jsonheader, id);
-//            assertTrue(true);
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//            assertTrue(false);
-//        }
+
+        String id = "did:axn:8d5c38a9-8ead-468d-b6d3-bec7d62341b0";
+
+        String strheader = "{\"Callback-Url\":\"http://something.com\"}";
+        JSONObject jsonheader = JSON.parseObject(strheader);
+
+        Client client = new Client();
+        client.Address = "IP:PORT";
+        client.ApiKey = "5zt592jTM1524126512";
+        client.CertPath = "/Users/yan/tmp/cert136";
+
+        Wallet wallet = new Wallet(client);
+
+        try {
+            wallet.Balance(jsonheader, id);
+            assertTrue(true);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            assertTrue(false);
+        }
     }
 }
